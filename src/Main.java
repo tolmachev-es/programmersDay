d614adf11886bdf3c879ffd730ed6e06273eb921
tolmachev.e.s@yandex.ru <tolmachev.e.s@yandex.ru>
2023-05-26 14:48:25 +0300

    Первая версия

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
