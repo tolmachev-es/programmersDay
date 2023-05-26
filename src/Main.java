068978f3ecfe2371e604804b7dbfc27fc05b60c1
tolmachev.e.s@yandex.ru <tolmachev.e.s@yandex.ru>
2023-05-26 14:57:31 +0300

    Первая версия

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,7 @@ public class Main {
     public static boolean isLeapYear(int year) {
         if (year % 4 == 0){
             if (year % 100 == 0){
-                if (year % 400 == 0){
-                    return true;
-                } else {
-                    return false;
-                }
+                return year % 400 == 0;
             } else {
                 return true;
             }
